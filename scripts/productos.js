d66d266a82becd8ab17188d0f6df9f85dbe51b79
Luis Altamiranda <la_107@hotmail.com>
2024-04-22 20:04:14 -0300

    Haciendo prueba en la página productos

---
+++ scripts/productos.js
@@ -0,0 +1,33 @@
+function cargarDatos(){
+    var titulo;
+    var texto;
+  
+    for (i=1;i<=Registros;i++)
+    {
+     titulo="Titulo"+i;
+     texto="Texto"+i;
+     document.getElementById(titulo).innerText=eval(titulo);
+     document.getElementById(texto).innerText=eval(texto);
+    }
+   }
+
+function escribirDatos(){
+   document.write("<table>");
+   for (i=1;i<=Registros;i++)
+   { 
+    document.write("<tr>");
+    document.write("<td id='Titulo"+i+"'></td>");
+    document.write("</tr>");
+  
+    document.write("<tr>");
+    document.write("<td id='Texto"+i+"'></td>");
+    document.write("</tr>");
+
+    document.write("<tr>");
+    document.write("<td><img src='./imagenes/articulo"+i+".jpg' alt='Imagen del artículo"+i+"' width='50px''></td>");
+    document.write("</tr>");
+
+    document.write("<tr><td>&nbsp;</td></tr>");
+   }
+   document.write("</table>");
+   }
